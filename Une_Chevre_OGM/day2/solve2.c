e48b392a835dffb68f9359a7a231aea0d65638dd
maelhos <52194580+maelhos@users.noreply.github.com>
2023-12-02 19:02:51 +0100

    part 2 done

--- Une_Chevre_OGM/day2/solve2.c
+++ Une_Chevre_OGM/day2/solve2.c
@@ -4,9 +4,13 @@
 #include <stdint.h>
 #include <assert.h>
 
-#define RED_DEFAULT   12
-#define BLUE_DEFAULT  14
-#define GREEN_DEFAULT 13
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+#define RED_DEFAULT   0
+#define BLUE_DEFAULT  0
+#define GREEN_DEFAULT 0
+
 
 int main(int argc, char* argv[]){
     FILE* fp = fopen("input", "r");
@@ -17,6 +21,10 @@ int main(int argc, char* argv[]){
     int32_t n_red = RED_DEFAULT;
     int32_t n_green = GREEN_DEFAULT;
 
+    int32_t nc_blue = BLUE_DEFAULT;
+    int32_t nc_red = RED_DEFAULT;
+    int32_t nc_green = GREEN_DEFAULT;
+
     int32_t n;
     uint8_t ok = 0;
     
@@ -25,24 +33,27 @@ int main(int argc, char* argv[]){
         n_red = RED_DEFAULT;
         n_green = GREEN_DEFAULT;
         
+        nc_blue = BLUE_DEFAULT;
+        nc_red = RED_DEFAULT;
+        nc_green = GREEN_DEFAULT;
+
         //printf("Game %03u :", game_nbr);
 
         ok = 0;
         while (!ok){
-            //printf(" w");
             assert(fscanf(fp, " %d ", &n) > 0);
 
             switch (fgetc(fp)){
             case 'b':
-                n_blue -= n;
+                nc_blue = MAX(nc_blue, n);
                 fseek(fp, 3, SEEK_CUR);
                 break;
             case 'r':
-                n_red -= n;
+                nc_red = MAX(nc_red, n);
                 fseek(fp, 2, SEEK_CUR);
                 break;
             case 'g':
-                n_green -= n;
+                nc_green = MAX(nc_green, n);
                 fseek(fp, 4, SEEK_CUR);
                 break;
             default:
@@ -54,40 +65,31 @@ int main(int argc, char* argv[]){
             case ',': 
                 break;
             case ';':
-                //printf(" [r: %d, g: %d, b: %d]", n_red, n_green, n_blue);
-                if (n_green < 0 || n_blue < 0 || n_red < 0){
-                    ok = 2;
-                    //printf(" nop");
-                }
-                else{
-                    ok = 0;
-                    //printf(" ok");
-                    n_blue = BLUE_DEFAULT;
-                    n_red = RED_DEFAULT;
-                    n_green = GREEN_DEFAULT;
-                }
+                n_red = MAX(nc_red, n_red);
+                n_green = MAX(nc_green, n_green);
+                n_blue = MAX(nc_blue, n_blue);
+
+                nc_blue = BLUE_DEFAULT;
+                nc_red = RED_DEFAULT;
+                nc_green = GREEN_DEFAULT;
+
                 break;
             case '\n':
-                //printf(" end");
-                if (n_green < 0 || n_blue < 0 || n_red < 0)
-                    ok = 3;
-                else
-                    ok = 1;
+                n_red = MAX(nc_red, n_red);
+                n_green = MAX(nc_green, n_green);
+                n_blue = MAX(nc_blue, n_blue);
+
+                ok = 1;
                 break;
             default:
                 assert(2 == 0);
                 break;
             }
         }
-        if (ok == 1){
-            //printf(" OK\n");
-            sum += game_nbr;
-        }
-        else{
-            if (ok == 2)
-                while (fgetc(fp) != '\n') {}
-            //printf(" NOP \n");
-        }
+
+        //printf(" [r: %d, g: %d, b: %d]\n", n_red, n_green, n_blue);
+        sum += n_red * n_green * n_blue;
+        
     }
     printf("SUM : %u\n", sum);
     fclose(fp);
